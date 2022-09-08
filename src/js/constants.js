c776771bceed7869c30afd3752490952b9c8b491
Sylvester Wilmott <sylvester.wilmott@gmail.com>
2022-09-08 23:59:42 +0100

    Update menu ui

--- src/js/constants.js
+++ src/js/constants.js
@@ -3,8 +3,6 @@
 export const IDLE_THRESHOLD      = 15;
 export const TIMER_DELAY         = 1;
 export const TIMER_PERIOD        = 1;
-export const STR_STATUS_ENABLED  = chrome.i18n.getMessage("status_enabled");
-export const STR_STATUS_DISABLED = chrome.i18n.getMessage("status_disabled");
 export const STR_PAUSE           = chrome.i18n.getMessage("pause");
 export const STR_UNPAUSE         = chrome.i18n.getMessage("unpause");
 export const STR_HOURS_SHORT     = chrome.i18n.getMessage("hours_short");
